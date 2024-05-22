b63f9d2d98f0045978fabcf49633c0ced1d16904
Daniel Chung <chung.dan.s@gmail.com>
2024-05-21 21:19:00 -0400

    Add indent animation

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -60,4 +60,42 @@ return {
       },
     },
   },
+  {
+    'echasnovski/mini.indentscope',
+    version = '*',
+    opts = { symbol = '│', options = { try_as_border = false } },
+    init = function()
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = {
+          'help',
+          'alpha',
+          'dashboard',
+          'neo-tree',
+          'Trouble',
+          'trouble',
+          'lazy',
+          'mason',
+          'notify',
+          'toggleterm',
+          'lazyterm',
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = false
+        end,
+      })
+    end,
+  },
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {
+      indent = {
+        char = '│',
+        tab_char = '│',
+      },
+      scope = {
+        enabled = false,
+      },
+    },
+  },
 }
